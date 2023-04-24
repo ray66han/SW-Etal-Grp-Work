3fabf68b7d38e758210a993e567230e9405fb870
Logic <madbeardedchicken@gmail.com>
2023-04-24 19:13:54 +0100

    Added functions to set chore info

--- Main/src/main/java/Main/Main.java
+++ Main/src/main/java/Main/Main.java
@@ -12,9 +12,20 @@ public class Main {
         DatabaseFunctions Database = new DatabaseFunctions(); // Create a database object we can use to interact with.
 
         // Getting Data for User1
-        User User1 = Database.GET_USER_WITH_ID(0); // Pulls a user from the datbase with ID 0 and makes it an object
-        Integer User1Total = Database.GET_USER_TOTAL_CHORES(User1); // Gets total users assigned chores.
-        Integer User1Completed = Database.GET_USER_COMPLETED_CHORES(User1); // Gets total users completed chores.
+        Chore Chore1 = Database.GET_CHORE_WITH_ID(1); // Pulls a user from the datbase with ID 0 and makes it an object
+        Database.SET_CHORE_NAME(Chore1, "New Chore Name/Desc!"); // Sets desc of chore
+        Database.SET_CHORE_STATUS(Chore1, 2); // Sets Status of Chore. E.g Completed
+        Database.SET_CHORE_TIME(Chore1, 50); // Sets the time of chore
+        Database.SET_CHORE_WEEKLY(Chore1, true);  // Is the chore weekly? Should it reoccur?
 
+        // Creating & Delete Chore
+
+        Database.CREATE_CHORE("Wash Towels", 50); // New chore of time 50 mintues.
+
+        Chore Chore2 = Database.GET_CHORE_WITH_ID(10);
+        User user = Database.GET_USER_WITH_ID(1);
+        Database.ASSIGN_CHORE_TO_USER(Chore2, user);
+        Database.DELETE_CHORE(Chore2);
     }
 }
+
